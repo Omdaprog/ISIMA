76da618879e17168f8543377fb464cd5fc5fdf8c
Omdaprog <imedrugby2306@gmail.com>
2020-09-28 22:27:50 +0100

    add class base view FormView

--- isima_trivez/forms.py
+++ isima_trivez/forms.py
@@ -1,9 +1,26 @@
+from django.forms import ModelForm
+from .models import PdfStore , PdfImages 
 from django import forms
 
-class UploadFrom(forms.Form):
+class UploadForm(ModelForm):
     title = forms.CharField()
-    matiere = forms.CharField()
+    matire = forms.CharField()
     degree = forms.CharField()
     description = forms.CharField()
     slug = forms.CharField()
-    image = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    
+
+    class Meta: 
+        model = PdfStore  
+        fields = ['title','matire','degree','description','slug']
+
+class UploadImages(forms.ModelForm):
+    pass
+#     # image = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+#     class Meta:
+#         model = PdfImages
+#         fields= ['image']
+
+#         widgets = {
+#             'image': forms.ClearableFileInput(attrs={'multiple': True}) 
+#         }
--- isima_trivez/views.py
+++ isima_trivez/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
-from .forms import UploadFrom
+from .forms import  UploadImages, UploadForm
 from .models import PdfStore, PdfImages
 # def homepage(request):
 #     form = UploadFrom()
@@ -21,26 +21,51 @@ from .models import PdfStore, PdfImages
 
 
 class homepage(FormView):
-    form_class = UploadFrom
-    template_name = 'base.html'  # Replace with your template.
-    # success_url = render(request,"base.html") # Replace with your URL or reverse().
+    form_class = UploadForm
+    template_name = 'base.html'
+     # Replace with your template.
+    success_url = '/' # Replace with your URL or reverse().
 
-    def post(self, request):
+    def form_valid(self, form):
+        print('form saved')
+        form.save()
+        return super().form_valid(form)
+
+
+
+    # def Post(self, request, *args, **kwargs):
+    #     print("post work fine")
+    #     form_class= self.get_form_class()
+    #     form = self.get_form(form_class)
+    #     files = request.FILES.getlist('image')
+    #     if form.is_valid():
+    #         for f in files: 
+    #             print(f)
+    #             f.save()
+    #         return render(request,"base.html")  
+    #     else:
+    #         return render(request,"base.html")
+
+
+
+
+
+    # def post(self, request):
         
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('image')
-        if form.is_valid():
-            pdfstore = form.save(commit=False)
-            pdfstore.save()
-            for f in files:
-                print(f)
-                file_instance = PdfImages(image=f)
-                file_instance.save() # https://github.com/Bhagyalakshmi18/Resume_django/blob/master/web_app/views.py
-            print('sucsessfuly posted')
-            return render(request,"base.html")
-        else:
-            return render(request,"base.html")
+    #     form_class = self.get_form_class()
+    #     form = self.get_form(form_class)
+    #     files = request.FILES.getlist('image')
+    #     if form.is_valid():
+    #         pdfstore = form.save(commit=False)
+    #         pdfstore.save()
+    #         for f in files:
+    #             print(f)
+    #             file_instance = PdfImages(image=f)
+    #             file_instance.save() # https://github.com/Bhagyalakshmi18/Resume_django/blob/master/web_app/views.py
+    #         print('sucsessfuly posted')
+    #         return render(request,"base.html")
+    #     else:
+    #         return render(request,"base.html")
 
 
 
